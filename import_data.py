ee66365d70ca7128b0b81c4d3de7f6bfd4216846
camillehebert <camille.hebert.4@ulaval.ca>
2022-03-24 15:03:27 -0400

    changes import_data

--- import_data.py
+++ import_data.py
@@ -13,9 +13,7 @@ def _import_data():
 	    try:
 	        if data_file[1][-3:]==str('csv'):
             	    data_p_values = pd.read_csv(file,delimiter=',', names=['Channel', 'Intensity'], skiprows = 21)
-    
        	            de = 20.1*(10**-3)
-        
         	    channel_p = data_p_values.Channel*de
               	    energy_p = (channel_p[channel_p<=24.0])
             	    intensity_p = (data_p_values.Intensity[channel_p<=24.0])
@@ -30,11 +28,8 @@ def _import_data():
         else:
             for file in dir_ls:
                     data_p_values = pd.read_csv(file,delimiter=',', names=['Channel', 'Intensity'], skiprows = 21)
-                
                     de = 19.8*(10**-3)
-              
                     energy_p = data_p_values.Channel*de
                     intensity_p = data_p_values.Intensity
-                     
                     database.append([energy_p, intensity_p])
             return database
